08e93ab238adf4fada362f5fd988394901cbcea4
Sawdes <lawerdaniil90@gmail.com>
2024-09-19 04:18:04 +0300

    try fix deploy bug for path

--- src/lib/convert.ts
+++ src/lib/convert.ts
@@ -3,6 +3,7 @@
 import { randomUUID } from "crypto";
 import { writeFile } from "fs/promises";
 import { revalidatePath } from "next/cache";
+import path from "path";
 import sharp from "sharp";
 
 export async function convert(formData: FormData) {
@@ -11,10 +12,8 @@ export async function convert(formData: FormData) {
   const buffer = new Uint8Array(arrayBuffer);
 
   const saveName = `${randomUUID()}-${Date.now().toString()}.webp`;
-  await sharp(buffer)
-    .resize(100, 100)
-    .webp()
-    .toFile("public/images/" + saveName);
+  const savePath = path.join(process.cwd(), "public", "images", saveName);
+  await sharp(buffer).resize(100, 100).webp().toFile(savePath);
   revalidatePath("/");
   return saveName;
 }
